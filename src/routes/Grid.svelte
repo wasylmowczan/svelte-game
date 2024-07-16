8f44773affe2506137d4bc24ef95279ae3669f0a
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-07-16 14:50:58 +0200

    Add logic for selecting squares

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -2,10 +2,34 @@
 	import Square from "./Square.svelte";
 
     export let grid: string[];
+
+    let a = -1;
+    let b = -1;
+    let reset_timeout: number;
 </script>
 <div class="grid">
-    {#each grid as emoji}
-        <Square {emoji}/>
+    {#each grid as emoji, i}
+        <Square {emoji} on:click={() => {
+            clearTimeout(reset_timeout);
+            
+            if (a === -1 && b ===-1) {
+                a = i;
+            } else if (b === -1) {
+                b=i;
+                if (grid[a] === grid[b]) {
+                    // correct
+                } else {
+                    reset_timeout = setTimeout(() => {
+                        a=b=-1
+                    }, 1000)
+                }
+            } else {
+                b = -1;
+                a = i;
+            }
+        }}
+        selected = {a === i || b ===i} 
+        />
     {/each}
 </div>
 
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     export let emoji: string;
+    export let selected: boolean;
 </script>
 
-<div class="square">
+<div class="square" class:flipped={selected}>
+    <button on:click />
     <span>{emoji}</span>
 </div>
 
@@ -14,7 +16,18 @@
         background: yellow;
     }
 
+    .flipped button{
+        background: red;
+    }
+
+    button {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+
     span {
         font-size: 5em;
+        pointer-events: none;
     }
 </style>
